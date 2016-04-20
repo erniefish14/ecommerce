29829c8259ef90e683c0c61a226912ac8258b954
erniefish14 <erniefish14@hotmail.com>
2016-04-21 07:55:59 +1000

    updated import usage to require

--- server/app.js
+++ server/app.js
@@ -4,38 +4,41 @@
 
 'use strict';
 
-import express from 'express';
-import mongoose from 'mongoose';
+var express = require('express');
+var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
-import config from './config/environment';
-import http from 'http';
+var config = require('./config/environment');
+// var http = require('http');
 
 // Connect to MongoDB
 mongoose.connect(config.mongo.uri, config.mongo.options);
-mongoose.connection.on('error', function(err) {
-  console.error('MongoDB connection error: ' + err);
-  process.exit(-1);
+mongoose.connection.on('error', function (err) {
+    console.error('MongoDB connection error: ' + err);
+    process.exit(-1);
 });
 
 // Populate databases with sample data
-if (config.seedDB) { require('./config/seed'); }
+if (config.seedDB) {
+    require('./config/seed');
+}
 
 // Setup server
 var app = express();
 var server = http.createServer(app);
 var socketio = require('socket.io')(server, {
-  serveClient: config.env !== 'production',
-  path: '/socket.io-client'
+    serveClient: config.env !== 'production',
+    path: '/socket.io-client'
 });
 require('./config/socketio').default(socketio);
+// setup expressjs - express js is a chain of middlewares that end in a route (processing cookies, sessions, logging, auth, etc)
 require('./config/express').default(app);
 require('./routes').default(app);
 
 // Start server
 function startServer() {
-  app.angularFullstack = server.listen(config.port, config.ip, function() {
-    console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
-  });
+    app.angularFullstack = server.listen(config.port, config.ip, function () {
+        console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+    });
 }
 
 setImmediate(startServer);
