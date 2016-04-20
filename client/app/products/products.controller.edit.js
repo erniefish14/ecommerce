c52dee8c2d7b4870ea74e406083c6b2f85977674
erniefish14 <erniefish14@hotmail.com>
2016-04-21 07:31:02 +1000

    Adding node js product components
    Adding node js product components

--- client/app/products/products.controller.edit.js
+++ client/app/products/products.controller.edit.js
@@ -1,26 +1,26 @@
 'use strict';
 
-(function() {
-	angular.module('meanshopApp').controller('ProductEditController',
-			ProductEditController);
+(function () {
+    angular.module('meanshopApp').controller('ProductEditController',
+        ProductEditController);
 
-	ProductEditController.$inject = [ 'Products', '$stateParams', '$state' ];
+    ProductEditController.$inject = ['Products', '$stateParams', '$state'];
 
-	function ProductEditController(Products, $stateParams, $state) {
-		var vm = this;
-		vm.editProduct = editProduct;
-		console.log('creating the product edit controller');
-		
-		vm.product = Products.get({
-			id : $stateParams.id
-		});
-		
-		function editProduct()	{
-			console.log('editing the current product');
-			Products.update(vm.product);
-			$state.go('products');
-		}
-	}
+    function ProductEditController(Products, $stateParams, $state) {
+        var vm = this;
+        vm.editProduct = editProduct;
+        console.log('creating the product edit controller');
+
+        vm.product = Products.get({
+            id: $stateParams.id
+        });
+
+        function editProduct() {
+            console.log('editing the current product');
+            Products.update(vm.product);
+            $state.go('products');
+        }
+    }
 
 })();
 
